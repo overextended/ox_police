03dd48d46069cac63a983fa282eda789837bdd16
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-12-21 19:07:25 +1100

    feat(client/main): notification on service state change

--- client/main.lua
+++ client/main.lua
@@ -2,8 +2,21 @@ local table = lib.table
 InService = player?.inService and table.contains(Config.PoliceGroups, player.inService) and player.hasGroup(Config.PoliceGroups)
 
 RegisterCommand('duty', function()
+    local wasInService = InService
     InService = not InService and player.hasGroup(Config.PoliceGroups) or false
-    TriggerServerEvent('ox:setPlayerInService', InService)
+
+    if not wasInService and not InService then
+        lib.notify({
+            description = 'Service not available',
+            type = 'error'
+        })
+    else
+        TriggerServerEvent('ox:setPlayerInService', InService)
+        lib.notify({
+            description = InService and 'In Service' or 'Out of Service',
+            type = 'success'
+        })
+    end
 end)
 
 AddEventHandler('ox:playerLogout', function()
